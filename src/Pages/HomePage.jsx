54ed35c5695ac748275d14c4b7bb5bc51e1ee1b4
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-03-07 11:55:14 +0600

    code write for hero section, use svg form font awesone site for arrow sign

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/navComponent/navBar'
+import Hero from '../components/heroComponent/Hero'
 
 const HomePage = () => {
   return (
     <div>
         <NavBar/>
+        <Hero/>
     </div>
   )
 }
