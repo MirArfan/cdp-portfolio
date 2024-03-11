411953b7bc1cf9a61c7b7f4d6a61335c56faa837
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-03-11 17:47:44 +0600

    packaga.json file change with homepage

--- src/components/navComponent/Resume.jsx
+++ src/components/navComponent/Resume.jsx
@@ -12,7 +12,7 @@ const Resume = () => {
 
   return (
     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse gap-2">
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700" onClick={toggleMenu}>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700" >
         Resume
       </button>
       <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700">
