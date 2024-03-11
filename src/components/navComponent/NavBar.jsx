ede8e2dc3642eac957920af2b8f3c59efee71c58
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-03-11 13:02:55 +0600

    navBar responsive code done

--- src/components/navComponent/NavBar.jsx
+++ src/components/navComponent/NavBar.jsx
@@ -5,16 +5,19 @@ import Logo from './Logo'
 
 const NavBar = () => {
     return (
-        <div className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Logo />
                 <NavList />
                 <Resume />
-                
-                
+
+
             </div>
         </div>
     )
 }
 
 export default NavBar
+
+
+
